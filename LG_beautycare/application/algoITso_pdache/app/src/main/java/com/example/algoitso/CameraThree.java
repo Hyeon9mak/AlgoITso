764042941ca42697cd159c787d38ae740a1fcb5d
Hyeon9mak <jinha3507@gmail.com>
2020-05-23 23:32:16 +0900

    ㅏ

---
+++ LG_beautycare/application/algoITso_pdache/app/src/main/java/com/example/algoitso/CameraThree.java
@@ -0,0 +1,14 @@
+package com.example.algoitso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_three);
+    }
+}
